400ead8f3a66798ecc867290f4ef3b116bf4cf21
Kishore <116268946+Kishore1607@users.noreply.github.com>
2023-07-13 18:39:19 +0530

    completed day 04 practice and solved

---
+++ src/main/java/com/fssa/corejava/day04/solved/Square.java
@@ -0,0 +1,13 @@
+package com.fssa.corejava.day04.solved;
+
+/**
+ * @author VinitGore *
+ */
+
+public class Square extends Rectangle {
+
+	public Square(double side_length) throws Exception {
+		super(side_length, side_length);
+	}
+
+}
