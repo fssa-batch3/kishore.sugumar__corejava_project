6868e7aa026fa1d1cf3c7e41822ca94a3ff9d631
Kishore <116268946+Kishore1607@users.noreply.github.com>
2023-09-09 21:28:12 +0530

    Implemented REST method for find all products in home page

--- src/main/java/com/fssa/corejava/day04/solved/AnotherClass.java
+++ src/main/java/com/fssa/corejava/day04/solved/AnotherClass.java
@@ -1,5 +1,10 @@
 package com.fssa.corejava.day04.solved;
 
-public class AnotherClass extends Square {  // This line will give compiler error
+public class AnotherClass extends Square {
+
+	public AnotherClass(double side_length) throws Exception {
+		super(side_length);
+		// TODO Auto-generated constructor stub
+	}  // This line will give compiler error
 
 }
